c8d1f5b565638894971fb7b770c71c97c24761e2
Brock Ericksen <brockericksen@gmail.com>
2022-12-13 22:24:37 -0500

    Final touches

--- server/models/carmeets.model.js
+++ server/models/carmeets.model.js
@@ -4,37 +4,37 @@ const CarMeetSchema = {
   meetName: {
     type: String,
     required: [true, "Name is required!"],
-    minLength: [3, "Name must be at least 3 characters!"],
+    minLength: [2, "Name must be at least 3 characters!"],
   },
 
   meetHost: {
     type: String,
     required: [true, "Host is required!"],
-    minLength: [3, "Host must be at least 3 characters!"],
+    minLength: [2, "Host must be at least 3 characters!"],
   },
 
   meetLocation: {
     type: String,
     required: [true, "Location is required!"],
-    minLength: [3, "Location must be at least 3 characters!"],
+    minLength: [2, "Location must be at least 3 characters!"],
   },
 
   meetDate: {
     type: String,
     required: [true, "Date is required!"],
-    minLength: [3, "Date must be in mm/dd/yyyy format!"],
+    minLength: [2, "Date must be in mm/dd/yyyy format!"],
   },
 
   meetTime: {
     type: String,
     required: [true, "Time is required!"],
-    minLength: [3, "Time for meet is required!"],
+    minLength: [2, "Time for meet is required!"],
   },
 
   description: {
     type: String,
     required: [true, "Description is required!"],
-    minLength: [3, "Description must be at least 3 characters!"],
+    minLength: [2, "Description must be at least 3 characters!"],
   },
 
   socials: {
